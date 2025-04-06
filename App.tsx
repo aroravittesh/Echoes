9200d56902f862617aa3cf71cfcc6df609ffafdd
unknown <7z.swarnim@gmail.com>
2025-04-06 11:51:45 +0530

    Finally

--- App.tsx
+++ App.tsx
@@ -1,20 +1,48 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// App.tsx
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import LoginScreen from "./Components/Login";
+import SignupScreen from "./Components/Signup";
+import MapScreen from "./Screens/MapScreen";
+import HistoryExplorer from "./Components/HistoryExplorer";
+import QuizScreen from "./Screens/Quiz";
+import PlacesVisitedScreen from "./Screens/PlacesVisited";
+import LeaderboardScreen from "./Screens/LeaderBoard";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen
+          name="Login"
+          component={LoginScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Signup"
+          component={SignupScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="HistoryExplorer"
+          component={HistoryExplorer}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen name="MapScreen" component={MapScreen} />
+        {
+          <>
+            <Stack.Screen name="Quiz" component={QuizScreen} />
+            <Stack.Screen
+              name="PlacesVisited"
+              component={PlacesVisitedScreen}
+            />
+            <Stack.Screen name="Leaderboard" component={LeaderboardScreen} />
+          </>
+        }
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
